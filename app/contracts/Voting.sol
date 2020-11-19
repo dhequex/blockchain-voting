4af805901d709d7a762ad7671aeedbb40af68095
Jose M. Torres <jmtorresr@gmail.com>
2020-11-19 15:36:57 +0900

    Modified migrations and contract to match deployer config

--- app/contracts/Voting.sol
+++ app/contracts/Voting.sol
@@ -2,96 +2,102 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Voting {
-    //This declares the Ethereum Address of the contract owner
-    address public owner;
+	//This declares the Ethereum Address of the contract owner
+	address public owner;
 
-    //These are state variables to store candidates and voters
-    uint256 internal numCandidates;
-    uint256 internal numVoters;
+	//These are state variables to store candidates and voters
+	uint256 internal numCandidates;
+	uint256 internal numVoters;
 
-    //Events will be used to inform the front-end of activity in our contract
-    event AddedCandidate(uint256 candidateId, string name, string proposal);
-    event NewVote(uint256 candidateId);
+	//Events will be used to inform the front-end of activity in our contract
+	event AddedCandidate(uint256 candidateId, string name, string proposal);
+	event NewVote(uint256 candidateId);
 
-    function vote() public {
-        owner = msg.sender;
-    }
+	function vote() public {
+		owner = msg.sender;
+	}
 
-    //This will be applied to the functions that should only be performed by the owner of the contract. As registering candidates.
-  modifier onlyOwner() {
-    require(isOwner(), "Not the owner");
-    _;
-  }
+	//This will be applied to the functions that should only be performed by the owner of the contract. As registering candidates.
+	modifier onlyOwner() {
+		require(isOwner(), "This operation is OnlyOwner");
+		_;
+	}
 
-      function isOwner() public view returns(bool) {
-    return msg.sender == owner;
-  }
-    //This will create a hash to assign an Id to a candidate and an address to a voter
-    mapping(uint256 => Candidate) public candidatesStore;
-    mapping(uint => Voter) public votersStore;
+	function isOwner() public view returns (bool) {
+		return msg.sender == owner;
+	}
 
-    //This defines the Voters structure.
-    struct Voter {
-        address ethAddress;
-        uint256 voterId;
-        uint256 candidateIdVote;
-    }
+	//This will create a hash to assign an Id to a candidate and an address to a voter
+	mapping(uint256 => Candidate) public candidatesStore;
+	mapping(uint256 => Voter) public votersStore;
 
-    //This defines the candidates structure
-    struct Candidate {
-        string name;
-        string proposal;
-        bool doesExist;
-    }
+	//This defines the Voters structure.
+	struct Voter {
+		address voterAddress;
+		uint256 voterId;
+		uint256 candidateIdVote;
+	}
 
-    //Here we add the functionality of our contract.
-    function addCandidate(string calldata name, string calldata proposal)
-        external
-        returns (uint256 candidateId)
-    {
-        candidateId = numCandidates++;
-        candidatesStore[candidateId] = Candidate(name, proposal, true);
-        emit AddedCandidate(candidateId, name, proposal);
-    }
+	//This defines the candidates structure
+	struct Candidate {
+		string name;
+		string proposal;
+		bool doesExist;
+	}
 
-    function addVote(
-        address ethAddress,
-        uint256 candidateIdVote,
-        uint256 voterId
-    ) public {
-        if (candidatesStore[candidateIdVote].doesExist = true) {
-            voterId = numVoters++;
-            votersStore[voterId] = Voter(
-                ethAddress,
-                voterId,
-                candidateIdVote
-            );
-            emit NewVote(candidateIdVote);
-        }
-    }
+	//Here we add the functionality of our contract.
+	function addCandidate(string calldata name, string calldata proposal)
+		external
+		returns (uint256 candidateId)
+	{
+		candidateId = numCandidates++;
+		candidatesStore[candidateId] = Candidate(name, proposal, true);
+		emit AddedCandidate(candidateId, name, proposal);
+	}
 
-    function totalVotes(uint candidateId) view public returns(uint){
-        uint numOfVotes = 0;
-        for(uint i = 0; i <numVoters; i++){
-            if(votersStore[i].candidateIdVote == candidateId){
-                numOfVotes++;
-            }
-        }
-        return numOfVotes; 
-    }
+	function addVote(
+		address voterAddress,
+		uint256 candidateIdVote,
+		uint256 voterId
+	) public {
+		if (candidatesStore[candidateIdVote].doesExist = true) {
+			voterId = numVoters++;
+			votersStore[voterId] = Voter(voterAddress, voterId, candidateIdVote);
+			emit NewVote(candidateIdVote);
+		}
+	}
 
-    function getNumOfCandidates() public view returns(uint){
-        return numCandidates;
-    }
+	function totalVotes(uint256 candidateId) public view returns (uint256) {
+		uint256 numOfVotes = 0;
+		for (uint256 i = 0; i < numVoters; i++) {
+			if (votersStore[i].candidateIdVote == candidateId) {
+				numOfVotes++;
+			}
+		}
+		return numOfVotes;
+	}
 
-    function getTotalVoters() public view returns(uint) {
-        return numVoters;
-    }
+	function getNumOfCandidates() public view returns (uint256) {
+		return numCandidates;
+	}
 
-    function getCandidate(uint candidateId) public view returns(uint, string memory, string memory) {
-            return (candidateId, candidatesStore[candidateId].name, candidatesStore[candidateId].proposal);
-    }
+	function getTotalVoters() public view returns (uint256) {
+		return numVoters;
+	}
 
-
+	function getCandidate(uint256 candidateId)
+		public
+		view
+		returns (
+			uint256,
+			string memory,
+			string memory
+		)
+	{
+		return (
+			candidateId,
+			candidatesStore[candidateId].name,
+			candidatesStore[candidateId].proposal
+		);
+	}
 }
-
